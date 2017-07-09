192e07e3f0eb23267b758d2912e2aab4ba98900e
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-07-08 22:06:07 -0700

    (feat) add modal view of company detail

--- public/src/main.js
+++ public/src/main.js
@@ -37,19 +37,69 @@
     };
   };
 
+  function makeNode(text, node) {
+    let h1 = document.createElement(node);
+    let innerText = document.createTextNode(text);
+    h1.appendChild(innerText);
+    return h1
+  }
+
+  function makeModal(company) {
+
+    console.log(company);
+
+    // modal
+    let box = document.createElement('div');
+    box.classList.add('modal-main');
+
+    // create darkened background
+    let background = document.createElement('div');
+    background.classList.add('modal-background');
+
+    // company info
+    let name = makeNode(company.name, 'h1');
+    let phone = makeNode(company.phone, 'h1');
+
+    // create close button
+    let close = makeNode('Close', 'div');
+    close.addEventListener('click', clearModal);
+    close.classList.add('modal-button');
+
+    box.appendChild(name);
+    box.appendChild(phone);
+    box.appendChild(close);
+
+    modal.appendChild(box);
+    modal.appendChild(background);
+  }
+
+  function makeResult(company) {
+    let result = document.createElement('li');
+    let text = document.createTextNode(company.name)
+    result.data = company;
+    result.addEventListener('click', function() { makeModal(this.data) });
+    result.appendChild(text);
+    results.appendChild(result);
+  }
+
   function makeResults(names) {
-    const html = names.map((name) => {
-      return `<li>${name.name}</li>`
-    }).join('');
-    results.innerHTML = html;
+    clearResults();
+    names.forEach((company) => {
+      makeResult(company);
+    })
   }
 
   function clearResults() {
     results.innerHTML = '';
   }
 
+  function clearModal() {
+    modal.innerHTML = '';
+  }
+
   const searchInput = document.querySelector('.search');
   const results = document.querySelector('.results');
+  const modal = document.querySelector('.modal');
 
   // add event listeners
   searchInput.addEventListener('change', debounce(getData, 1000));
